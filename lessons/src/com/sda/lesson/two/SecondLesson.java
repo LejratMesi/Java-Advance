f7e24b5644475fdcda5df711a4ba08b7b28cf7d0
Lejrat Mesi <lejrat.mesi@sisal.al>
2022-03-20 11:30:05 +0100

    Example with Array, Inteface, methods

--- lessons/src/com/sda/lesson/two/SecondLesson.java
+++ lessons/src/com/sda/lesson/two/SecondLesson.java
@@ -1,4 +1,13 @@
 package com.sda.lesson.two;
 
 public class SecondLesson {
+    public static void main(String[] args) {
+        SecondLessonImpl exercise = new SecondLessonImpl(new Integer[]{1,2,3,4,5},
+                new String[]{"LEjraR","rrrr","mesi"});
+
+        System.out.println("Maximumi eshte +" + exercise.findMax());
+        exercise.findSum(4,10);
+        exercise.printWordsThatEndsWithLetterR();
+    }
+
 }
---
+++ lessons/src/com/sda/lesson/two/SecondLessonImpl.java
@@ -0,0 +1,44 @@
+package com.sda.lesson.two;
+
+public class SecondLessonImpl implements SecondLeson{
+    //Ka 2 members
+    private Integer numrat[];
+    private String emrat[];
+
+    public SecondLessonImpl(Integer[] numrat, String[] emrat) {
+        this.numrat = numrat;
+        this.emrat = emrat;
+    }
+
+    @Override
+    public Integer findMax() {
+        Integer max = numrat[0];
+        //bredhjen e array-it
+        for (int i = 1;i< numrat.length;i++){
+            if(numrat[i]> max){
+                max = numrat[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public void findSum(Integer number, Integer number2) {
+            Integer sum = 0;
+            for (int i = number;i<=number2;i++){
+                sum += i;
+            }
+
+        System.out.println("Shuma eshte = " + sum);
+    }
+
+    @Override
+    public void printWordsThatEndsWithLetterR() {
+        for (int i = 0;i< emrat.length;i++){
+            if(emrat[i].endsWith("R")){
+                System.out.println(emrat[i]);
+            }
+        }
+    }
+}
