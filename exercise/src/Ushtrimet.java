30ed56c807838f9d1b34ffd73957c8d180ff8a70
Lejrat Mesi <lejrat.mesi@sisal.al>
2022-05-07 15:04:02 +0200

    Coding exercise first Day. Exercise 1 to 5 and some other exercise from the trainer

---
+++ exercise/src/Ushtrimet.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ushtrimet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String endWord = "grupi_1";
+
+        while (!endWord.equals("end")){
+            System.out.println(endWord + " ka ushtrimin " +( new Random().nextInt(7)+5));
+            endWord = input.next();
+        }
+    }
+}
